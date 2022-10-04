833b2d7b448e0194db89b9821d6da8dd53494826
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-10-05 01:43:49 +0200

    ADD: [PING] the first route

--- src/app.py
+++ src/app.py
@@ -1,14 +1,18 @@
 import falcon
 
+from src.routes import Ping
+
 
 class GalleryWebAPP:
 
     def __init__(self) -> None:
         self._app: falcon.App = falcon.App(cors_enable=True)
 
+        self.ping: Ping = Ping()
+
     @property
     def app(self) -> falcon.App:
         return self._app
 
     def add_routes(self) -> None:
-        pass
+        self._app.add_route("/ping", self.ping)
