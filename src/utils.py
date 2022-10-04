6ff9d8034a133fddb6dca89612fb2b46a954a902
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-10-05 01:42:20 +0200

    ADD: basic mimetypes enum

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+from enum import Enum
 from typing import Tuple
 
 
@@ -8,3 +9,22 @@ def parse_dict_to_json_bytes(dictionary: dict) -> Tuple[bytes, int]:
         encoding="utf-8"
     )
     return byte_json, len(byte_json)
+
+
+class MimeTypes(Enum):
+    # Dynamic HTML files
+    JS = "text/javascript"
+    HTML = "text/html"
+    ICO = "image/vnd.microsoft.icon"
+    CSS = "text/css"
+
+    JSON = "application/json"
+
+    # image formats
+    AVIF = "image/avif"
+    GIF = "image/gif"
+    JPG = JPEG = "image/jpeg"
+    PNG = "image/png"
+    SVG = "image/svg+xml"
+    TIFF = "image/tiff"
+    WEBP = "image/webp"
