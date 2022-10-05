db8c2ac4309975c9c08eba1c3d71305e12dee8a3
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-10-05 02:24:26 +0200

    ADD dhtml get file by name method

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,10 +1,11 @@
 from typing import Dict, Tuple
 
+import falcon.errors
 from falcon import Request
 from falcon import Response
 
 from src.utils import parse_dict_to_json_bytes, MimeTypes
-from storages.dhtml_storage import DHTMLFolder
+from storages.dhtml_storage import DHTMLFolder, DHTMLFile
 
 
 class Ping:
@@ -25,6 +26,7 @@ class Ping:
 class DHTMLFilesRoute:
     def __init__(self, dhtml_component: DHTMLFolder) -> None:
         self.component: DHTMLFolder = dhtml_component
+        self.mime_type: str = dhtml_component.mime_type
 
     def on_get(self, req: Request, resp: Response) -> None:
         message: Dict[str, str] = {self.component.name: self.component.items}
@@ -33,3 +35,18 @@ class DHTMLFilesRoute:
         )
         resp.data, resp.content_length = parsed_message
         resp.content_type = MimeTypes.JSON.value
+
+    def on_get_item(self, req: Request, resp: Response, name: str) -> None:
+        if self.validate_item(name) is False:
+            raise falcon.errors.HTTPBadRequest
+        item: DHTMLFile = self.component["name"]
+        resp.data = item.file_bytes
+        resp.content_length = item.file_bytes_len
+        resp.content_type = MimeTypes.JSON.value
+
+    def validate_item(self, name: str) -> bool:
+        if 20 < len(name):
+            return False
+        if name not in self.component:
+            return False
+        return True
