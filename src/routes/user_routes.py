0eb006ecf48bc56bfd7d3f08d37263d0e1a1fa81
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-10-05 02:11:57 +0200

    ADD dhtml files get all route

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -3,7 +3,8 @@ from typing import Dict, Tuple
 from falcon import Request
 from falcon import Response
 
-from src.utils import parse_dict_to_json_bytes
+from src.utils import parse_dict_to_json_bytes, MimeTypes
+from storages.dhtml_storage import DHTMLFolder
 
 
 class Ping:
@@ -19,3 +20,16 @@ class Ping:
     def on_get(self, req: Request, resp: Response) -> None:
         resp.data, resp.content_length = self.parsed_message
         resp.content_type = self.DEFAULT_CONTENT_TYPE
+
+
+class DHTMLFilesRoute:
+    def __init__(self, dhtml_component: DHTMLFolder) -> None:
+        self.component: DHTMLFolder = dhtml_component
+
+    def on_get(self, req: Request, resp: Response) -> None:
+        message: Dict[str, str] = {self.component.name: self.component.items}
+        parsed_message: Tuple[bytes, int] = parse_dict_to_json_bytes(
+            message
+        )
+        resp.data, resp.content_length = parsed_message
+        resp.content_type = MimeTypes.JSON.value
