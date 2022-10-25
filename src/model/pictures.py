fbc35d8e5100e1219a5607ebbbe486d4d9925590
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-10-25 21:39:34 +0200

    ADD: override eq ne hash dunders in picture

--- src/model/pictures.py
+++ src/model/pictures.py
@@ -9,6 +9,18 @@ class Picture:
         self.name: str = name
         self.alt: str = alt
 
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Picture):
+            if self.name == other.name and self.alt == other.alt:
+                return True
+        return False
+
+    def __ne__(self, other: object) -> bool:
+        return not self.__eq__(other)
+
+    def __hash__(self) -> int:
+        return hash(self.to_dict())
+
     def to_dict(self) -> Dict[str, str]:
         return {
             "name": self.name,
@@ -19,3 +31,6 @@ class Picture:
 class Pictures:
     def __init__(self) -> None:
         self.pictures: List[Picture] = []
+
+    def add_picture(self, picture: Picture) -> None:
+        self.pictures.append(picture)
