6583d98897940c363779d9c90bbe355c92057147
SzakacsAdam <szakacs.adam.98@gmail.com>
2022-10-25 20:40:05 +0200

    ADD: Json and dict file handling support

--- src/markhor_db/markhor_db.py
+++ src/markhor_db/markhor_db.py
@@ -1,3 +1,8 @@
+from json import dumps as json_dumps
+from json import loads as json_loads
+from typing import Any
+from typing import Dict
+
 from aiofiles import open as aio_open
 
 
@@ -14,3 +19,12 @@ class _FileHandling:
     async def save_file(self, data: str) -> None:
         async with aio_open(self._src, mode='w+', encoding="UTF-8") as f:
             await f.write(data)
+
+    async def load_json(self) -> Dict[str, Any]:
+        content: str = await self.load_file()
+        return json_loads(content)
+
+    async def save_dict(self, data: Dict[str, Any]) -> None:
+        json_data: str = json_dumps(data, ensure_ascii=True, indent=4,
+                                    sort_keys=True, encoding="UTF-8")
+        await self.save_file(json_data)
